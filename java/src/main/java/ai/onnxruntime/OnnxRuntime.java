236d2ded19d4db4f5ca2e0c7046794a08a894807
daquexian <daquexian566@gmail.com>
2020-02-16 20:04:05 +0800

    Remove redundant code

--- java/src/main/java/ai/onnxruntime/OnnxRuntime.java
+++ java/src/main/java/ai/onnxruntime/OnnxRuntime.java
@@ -10,7 +10,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
